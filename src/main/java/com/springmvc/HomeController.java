cc7be0f3f5580ba36985f6a6eaecc24819d48bfc
seema-chaudhary <seemaee.ch@gmail.com>
2023-09-13 13:08:16 +0530

    first commit added image and static files

---
+++ src/main/java/com/springmvc/HomeController.java
@@ -0,0 +1,99 @@
+package com.springmvc;
+
+import java.io.File;
+import java.io.FileOutputStream;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
+import org.springframework.web.servlet.view.RedirectView;
+
+import com.springmvc.entity.User;
+import com.springmvc.service.UserService;
+
+@Controller
+public class HomeController {
+	
+	@Autowired
+	private UserService userService;
+	
+	@RequestMapping("/home")
+	public String home(Model m)
+	{
+		m.addAttribute("name", "Seema");
+		m.addAttribute("id", 101);
+		
+		return "home";
+	}
+
+		
+	@RequestMapping("/register")
+	public String signup()
+	{		
+		return "register";
+	}
+	
+	@RequestMapping(path="/createUser", method= RequestMethod.POST)
+	public String registerUser(@ModelAttribute User user, @RequestParam("img") String img,Model m )
+	{
+		user.setImage(img);
+		userService.saveUser(user);
+		System.out.println(user);
+		System.out.println("Thanku...");
+		return "success";
+	}
+	
+//	Redirection Way-1
+	@RequestMapping("/google1")
+	public String redirectPage()
+	{		
+		return "redirect:https://www.google.com";
+	}
+	
+	
+//	Redirection Way-2
+	@RequestMapping("/google2")
+	public RedirectView redirectPage1()
+	{		
+		RedirectView redview = new RedirectView();
+		redview.setUrl("https://www.google.com");
+		return redview;
+	}
+
+	
+//	Search Engine
+	@RequestMapping(path="/search", method=RequestMethod.POST)
+	public String search(@RequestParam("keyword") String keyword)
+	{		
+		String url="https://www.google.com/search?q="+keyword;
+		return "redirect:"+url;
+	}
+	
+	
+////	@PathVariable Example for single Value
+//	@RequestMapping("/user/{id}")
+//	public String search(@PathVariable("id")int id)
+//	{		
+//		System.out.println("Id = "+ id);
+//		return "home";
+//	}
+	
+//	@PathVariable Example for double Value
+	@RequestMapping("/user/{id}/{name}")
+	public String search(@PathVariable("id")int id, @PathVariable String name)
+	{		
+		System.out.println("Id = "+ id);
+		System.out.println("Name = "+ name);
+		return "home";
+	}
+	
+	
+}
---
+++ src/main/webapp/WEB-INF/view/home.jsp
@@ -0,0 +1,66 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+	pageEncoding="ISO-8859-1"%>
+	<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Home</title>
+<link
+	href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+	rel="stylesheet"
+	integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
+	crossorigin="anonymous">
+<link rel="stylesheet" href="<c:url value="/resources/css/style.css"/>">
+<script type="text/javascript"
+	src="<c:url value="/resources/js/script.js"/>"></script>
+</head>
+<body style="background-color: #f0f0f0">
+	<div class="text-center">
+		<hr>
+		<h1 class="">Home page</h1>
+		<a href="register" class="btn btn-primary btn-sm">Register</a> <a
+			href="google1" class=" btn btn-sm btn-danger">Google-1</a> <a
+			href="google2" class=" btn btn-sm btn-warning">Google-2</a>
+		
+			
+			<div class="container p-5">
+			<div class="row">
+				<div class="col-md-8 offset-md-2">
+					<div class="card">
+						<div class="card-body">
+							<h4>My Search Engine</h4>
+							<form action="search" method="post">
+								<div class="mb-3">
+									<input type="text" name="keyword" class="form-control"
+										placeholder="Enter keyword">
+									<button class="btn btn-sm btn-primary mt-3">Search
+										Google</button>
+								</div>
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="text-center">
+			<br>
+		<img alt="" src="<c:url value="/resources/img/SQL.jpg"/>" border="1px" width="500px" height="250px">
+		</div>
+	</div>
+	
+	<hr>
+	
+		<%
+		String name = (String) request.getAttribute("name");
+		Integer id = (Integer) request.getAttribute("id");
+		%>
+		<h2>
+			Welcome,
+			<%=name%>
+		</h2>
+		<h2>
+			ID:
+			<%=id%>
+		</h2>
+</body>
+</html>
